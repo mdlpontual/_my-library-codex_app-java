795eaa5eb3317008702d639459821b21a5720165
mdl.al <mdlpontual@gmail.com>
2025-08-26 18:47:59 -0300

    devolving service adds

--- src/service/BookService.java
+++ src/service/BookService.java
@@ -1,16 +1,24 @@
 package service;
 
 import model.items.Book;
+import repository.impl.SimulatedAuthorRepository;
 import repository.impl.SimulatedBookRepository;
+import repository.impl.SimulatedGenreRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookService {
     private final SimulatedBookRepository bookRepository;
+    private final SimulatedAuthorRepository authorRepository;
+    private final SimulatedGenreRepository genreRepository;
 
-    public BookService(SimulatedBookRepository bookRepository) {
+    public BookService(SimulatedBookRepository bookRepository,
+                       SimulatedAuthorRepository authorRepository,
+                       SimulatedGenreRepository genreRepository) {
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.genreRepository = genreRepository;
     }
 
     public List<Book> listAll() {
@@ -32,6 +40,23 @@ public class BookService {
     }
 
     public void addBook(Book newBookEntry) {
+        if (newBookEntry.getTitle() == null || newBookEntry.getTitle().length() < 2) {
+            throw new IllegalArgumentException("Invalid title input.");
+        } else if (bookRepository.listAll()
+                    .stream()
+                    .anyMatch(n -> n.getTitle().trim()
+                    .equalsIgnoreCase(newBookEntry.getTitle().toLowerCase().trim()))) {
+            throw new IllegalArgumentException("This title already have an entry.");
+        }
 
+        if (newBookEntry.getAuthor().getName() == null || newBookEntry.getAuthor().getName().length() < 2) {
+            throw new IllegalArgumentException("Invalid author name.");
+        } else if (authorRepository.listAll()
+                .stream()
+                .anyMatch(n -> n.getName().trim()
+                        .equalsIgnoreCase(newBookEntry.getAuthor().getName().toLowerCase().trim()))) {
+            // if any match I will need to swap the input one for the one in the db
+            // else i add to the database
+        }
     }
 }
