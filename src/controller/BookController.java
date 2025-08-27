54dd52792757854c98f94dfeba8694ca4d637239
mdl.al <mdlpontual@gmail.com>
2025-08-27 18:23:35 -0300

    Controller start + listAll()

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import model.items.Book;
+import repository.impl.SimulatedAuthorRepository;
+import repository.impl.SimulatedBookRepository;
+import repository.impl.SimulatedGenreRepository;
+import service.BookService;
+import util.DatabaseSimulator;
+
+import java.util.List;
+
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    public List<Book> listAll() {
+        return bookService.listAll();
+    }
+
+    public void findById() {
+
+    }
+
+    public void findByTitle() {
+
+    }
+
+    public void addBook() {
+
+    }
+
+    public void removeById() {
+
+    }
+
+    public void removeByTitle() {
+
+    }
 }
