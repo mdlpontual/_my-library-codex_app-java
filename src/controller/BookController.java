a417c83a1751c41e9003bea2cbefb11157dca387
mdl.al <mdlpontual@gmail.com>
2025-08-27 18:40:11 -0300

    populate controller methods - findByID, findByTitle and addBook

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.attributes.Author;
+import model.attributes.Genre;
 import model.items.Book;
 import repository.impl.SimulatedAuthorRepository;
 import repository.impl.SimulatedBookRepository;
@@ -8,6 +10,7 @@ import service.BookService;
 import util.DatabaseSimulator;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class BookController {
     private final BookService bookService;
@@ -21,15 +24,35 @@ public class BookController {
     }
 
     public void findById() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert the ID you are searching for:");
+        Integer idInput = scanner.nextInt();
+        bookService.findById(idInput);
     }
 
     public void findByTitle() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert the title of the book you are searching for:");
+        String titleInput = scanner.nextLine();
+        bookService.findByTitle(titleInput);
     }
 
     public void addBook() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("First, insert the title of the book you want to add to the collection: ");
+        String titleInput = scanner.nextLine();
+
+        System.out.println("Now insert the name of the author: ");
+        String authorNameInput = scanner.nextLine();
+        Author authorInput = new Author(authorNameInput);
+
+        System.out.println("Insert the name of the genre to finish: ");
+        String genreNameInput = scanner.nextLine();
+        Genre genreInput = new Genre(genreNameInput);
+
+        Book newBookEntry = new Book(titleInput, authorInput, genreInput);
+        bookService.addBook(newBookEntry);
     }
 
     public void removeById() {
