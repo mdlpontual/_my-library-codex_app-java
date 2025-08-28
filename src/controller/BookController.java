3bb82e81f8770d1bb9ee8afd3fac5dfea6714ac8
mdl.al <mdlpontual@gmail.com>
2025-08-27 21:52:55 -0300

    finish the controller class and its methods - returns and try/catch operations

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -6,6 +6,7 @@ import model.items.Book;
 import service.BookService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class BookController {
@@ -24,7 +25,10 @@ public class BookController {
         System.out.println("Insert the ID you are searching for:");
         Integer idInput = scanner.nextInt();
         scanner.nextLine();
+
         try {
+            // we need to handle a null return here because we can get a null value returned from the database
+            // if we look all the way back on the repository method, we can have a null value returned
             Book b = bookService.findById(idInput);
             if (b == null) {
                 System.out.println("No book found with ID " + idInput);
@@ -40,6 +44,8 @@ public class BookController {
         System.out.println("Insert the title of the book you are searching for:");
         String titleInput = scanner.nextLine();
         try {
+            // we need to handle a null return here because we can get a null value returned from the database
+            // if we look all the way back on the repository method, we can have a null value returned
             Book b = bookService.findByTitle(titleInput);
             if (b == null) {
                 System.out.println("No book found with title " + titleInput);
@@ -65,7 +71,7 @@ public class BookController {
 
         Book newBookEntry = new Book(titleInput, authorInput, genreInput);
 
-        // the try block here don't need to take care of null because is adding,
+        // the try block here don't need to take care of null because it is adding,
         // therefore it doesn't have the risk of returning null from the database
         // what in the find methods is the case, here is not
         try {
@@ -77,31 +83,62 @@ public class BookController {
 
     public String removeById() {
         System.out.println("Insert the ID of the book you want to remove from the collection:");
-        Integer idInput = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Are you sure you want to delete this item?");
-        System.out.println("Type 0 to CONFIRM, or type 1 to CANCEL the operation:");
-        int confirmInt = scanner.nextInt();
-        scanner.nextLine();
-        if (confirmInt == 1) {
-            return "Operation canceled.";
+        // try/catch avoids crash in case the input here is non-numeric
+        int idInput;
+        try {
+            idInput = scanner.nextInt();
+        } catch (Exception e) {
+            scanner.nextLine();
+            return "Invalid ID. Operation canceled.";
         }
-        return bookService.removeById(idInput);
+        scanner.nextLine();
+
+        // second confirmation before deleting entry - safety net
+        System.out.println("Are you sure you want to delete this item?");
+        System.out.println("Type Y to CONFIRM, or type N to CANCEL the operation:");
+        String confirm = scanner.nextLine().toUpperCase().trim();
+
+        // if the user types "Y" the code goes to the try/catch to process the operation - if not:
+        if ("N".equals(confirm)) {
+            return "Operation canceled.";
+        } else if (!"Y".equals(confirm)) {
+            return "Invalid value. Operation canceled.";
+        }
+
+        // the try block allows the trowing of the exception (in the service method) in case it fails
+        // then this exception from the service is catch here, and handled accordingly
+        // the "e.getMessage()" picks the message I customized in the throw from the service method
+        try {
+            return bookService.removeById(idInput);
+        } catch (IllegalArgumentException | NoSuchElementException e) {
+            return "Error: " + e.getMessage();
+        }
     }
 
     public String removeByTitle() {
         System.out.println("Insert the title of the book you want to remove from the collection:");
+        // not need try/catch because strings normally don't fail for bad input - it accepts ints as strings after all
         String titleInput = scanner.nextLine();
-        System.out.println("Are you sure you want to delete this item?");
-        System.out.println("Type 0 to CONFIRM, or type 1 to CANCEL the operation:");
-        int confirmInt = scanner.nextInt();
-        scanner.nextLine();
 
-        if (confirmInt == 1) {
+        // second confirmation before deleting entry - safety net
+        System.out.println("Are you sure you want to delete this item?");
+        System.out.println("Type Y to CONFIRM, or type N to CANCEL the operation:");
+        String confirm = scanner.nextLine().toUpperCase().trim();
+
+        // if the user types "Y" the code goes to the try/catch to process the operation - if not:
+        if ("N".equals(confirm)) {
             return "Operation canceled.";
-        } else if (confirmInt > 1) {
+        } else if (!"Y".equals(confirm)) {
             return "Invalid value. Operation canceled.";
         }
-        return bookService.removeByTitle(titleInput);
+
+        // the try block allows the trowing of the exception (in the service method) in case it fails
+        // then this exception from the service is catch here, and handled accordingly
+        // the "e.getMessage()" picks the message I customized in the throw from the service method
+        try {
+            return bookService.removeByTitle(titleInput);
+        } catch (IllegalArgumentException | NoSuchElementException e) {
+            return "Error: " + e.getMessage();
+        }
     }
 }
